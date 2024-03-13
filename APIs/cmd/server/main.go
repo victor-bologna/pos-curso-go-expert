5a3e2740d17b37aa259bb58e37703b0d03d4352a
victor-bologna <victorbologna@hotmail.com>
2024-03-13 16:05:48 -0300

    Added product routes

--- APIs/cmd/server/main.go
+++ APIs/cmd/server/main.go
@@ -1,8 +1,37 @@
 package main
 
-import "github.com/victor-bologna/pos-curso-go-expert-apis/configs"
+import (
+	"net/http"
+
+	"github.com/glebarez/sqlite"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"github.com/victor-bologna/pos-curso-go-expert-apis/configs"
+	"github.com/victor-bologna/pos-curso-go-expert-apis/internal/entity"
+	"github.com/victor-bologna/pos-curso-go-expert-apis/internal/infra/database"
+	"github.com/victor-bologna/pos-curso-go-expert-apis/internal/infra/webservers/handlers"
+	"gorm.io/gorm"
+)
 
 func main() {
-	config := configs.LoadConfig(".")
-	println(config)
+	_ = configs.LoadConfig(".")
+
+	db, err := gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
+	if err != nil {
+		panic(err)
+	}
+	db.AutoMigrate(&entity.User{}, &entity.Product{})
+
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+
+	productDB := database.NewProductDB(db)
+	productHandler := handlers.NewProductHandler(productDB)
+
+	r.Post("/products", productHandler.CreateProduct)
+	r.Get("/products/{id}", productHandler.FindProductByID)
+	r.Get("/products", productHandler.FindAllProducts)
+	r.Put("/products/{id}", productHandler.UpdateProduct)
+	r.Delete("/products/{id}", productHandler.DeleteProduct)
+	http.ListenAndServe(":8000", r)
 }
